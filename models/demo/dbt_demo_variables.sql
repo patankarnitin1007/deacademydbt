49b24b7381757ef1734b91baeaaf9aa27a880878
Nick P <patankar.nitin@gmail.com>
2026-06-24 23:00:28 +0000

    Changes

---
+++ models/demo/dbt_demo_variables.sql
@@ -0,0 +1,9 @@
+{%- set name=['Nick','Jay','Sach'] -%}
+
+    {%- for i in name -%}
+        {%- if i =='Nick' -%}
+            Hi {{ i }}.
+        {% else %}
+            Bye  {{ i }}.
+        {%- endif -%}
+    {%- endfor -%}        
